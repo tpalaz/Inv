9d580178fda30e8b52380e90cd6393db131a7708
tpalaz <routaruo@outlook.com>
2018-02-12 19:11:09 -0700

    stylechange/marketmarquee

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def contact_email
+    UserMailer.contact_email
+  end
+end
